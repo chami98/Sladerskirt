e6098dc9c1a833a21cabf4b587a4e1cd2a257a18
chami98 <chamikara98@gmail.com>
2022-09-06 10:41:40 +0530

    Created ImageContainer Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 const Header = () => {
   return (
     <div className="container mt-4" style={{ fontFamily: "Roboto" }}>
---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ImageContainer = () => {
+  return <div>
+        <p>hello there</p>
+  </div>;
+};
+
+export default ImageConatiner;
